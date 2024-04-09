ce8e67f2549841933417a7323eae5540233308cc
Shay Ronen <shayronen@gmail.com>
2024-04-09 10:54:52 +0300

    Create pricing-script.js

---
+++ pricing-script.js
@@ -0,0 +1,312 @@
+<body>
+  <div class="top-main">
+    <div class="container">
+    <div class="top">
+      <h3>Pricing Designed for Your Success!</h3>
+      <div class="toggle-btn">
+        <span style="margin: 0.8em; font-size:19px; font-weight:900;">Monthly</span>
+        <label class="switch">
+          <input type="checkbox" id="checbox" onclick="check()" ; />
+          <span class="slider round"></span>
+        </label>
+        <span style="margin: 0.8em; font-size:19px; font-weight:900;">Annually</span></div>
+    </div>
+
+    <div class= "main">
+
+    <div class="package-container">
+      <div class="packages" style="padding-top:20px; padding-bottom:190px;">
+        <h1>Starter</h1>
+        <h2 class="text2">&dollar;89<span style="font-size:19px;">/month</span></h2>
+        <a href="#" class="button button1">Start Now</a>
+        <ul class="list">
+         <li class="first">Cloud Based solution</li>
+          <li class="sec" style="border-bottom: 1px solid black;">Automated LinkedIn Outreach</li>
+           <p>Ideal for Startups & Personal Brands</p>
+           <li>Smart sequence outreach campaigns</li>
+ 	   <li>Multi-channel experience</li>
+  	  <li>Connect your unique email provider</li>
+ 	  <li>Lead Duplication Security</li>
+	  <li>Advanced Reporting</li>
+	  <li>Hyper Personalization with graphics and GIFs</li>
+	  <li>Basic Level Support</li>
+ 	  <li>Dynamic Placeholders</li>
+	  <li>Automation Scheduler</li>
+	  <li>Dedicated IP for each account</li>
+	  <li>Smart Limits</li>
+	  <li>Cloud Based solution</li>
+	  <li>Auto Warm up feature</li>
+	  <li>Auto Withdrawal of pending invites</li>
+        </ul>
+        
+      </div>
+     
+      <div class="packages" style="padding-top:20px; padding-bottom:70px;">
+        <h4 style="background-color:#F1B000; width:100%; color:white; font-weight:400; font-size:23px; padding:15px 0p; margin:0;">Most popular</h4>
+        <h1>Growth</h1>
+        <h2 class="text2">&dollar;99<span style="font-size:19px;">/month</span></h2>
+        <a href="#" class="button button2">Start Now</a>
+        <ul class="list">
+       <li class="first">5,000 chats</li>
+          <li class="sec" style="border-bottom: 1px solid black;">Unlimited users</li>
+           <p>Optimize Lead Gen Success for B2B Marketers</p>
+           <li>Smart sequence outreach campaigns</li>
+ 	   <li>Multi-channel experience</li>
+  	  <li>Connect your unique email provider</li>
+ 	  <li>Lead Duplication Security</li>
+	  <li>Advanced Reporting</li>
+	  <li>Hyper Personalization with graphics and GIFs</li>
+	  <li>Elite Level Support</li>
+ 	  <li>Dynamic Placeholders</li>
+	  <li>Automation Scheduler</li>
+	  <li>Dedicated IP for each account</li>
+	  <li>Smart Limits</li>
+	  <li>Automated LinkedIn Outreach</li>
+	  <li>Cloud Based solution</li>
+	  <li>Auto Warm up feature</li>
+	  <li>Auto Withdrawal of pending invites</li>
+        </ul>
+        
+      </div>
+      <div class="packages" style="padding-top:20px; padding-bottom:7px;">
+        <h1>Enterprise</h1>
+        <h2 class="text2">Let's Talk</h2>
+         <a href="#" class="button button3">Start Now</a>
+        <ul class="list">
+        <li class="first">10,000 chats</li>
+          <li class="sec" style="border-bottom: 1px solid black;">Unlimited users</li>
+           <p>Unlock New Revenue Streams for Agencies.</p>
+           <li>Smart sequence outreach campaigns</li>
+ 	   <li>Multi-channel experience</li>
+  	  <li>Connect your unique email provider</li>
+ 	  <li>Lead Duplication Security</li>
+	  <li>Advanced Reporting</li>
+	  <li>Hyper Personalization with graphics and GIFs</li>
+	  <li>Elite Level Support</li>
+ 	  <li>Dynamic Placeholders</li>
+	  <li>Automation Scheduler</li>
+	  <li>Dedicated IP for each account</li>
+	  <li>Smart Limits</li>
+	  <li>Automated LinkedIn Outreach</li>
+	  <li>Cloud Based solution</li>
+	  <li>Auto Warm up feature</li>
+	  <li>Auto Withdrawal of pending invites</li>
+        </ul>
+       
+      </div>
+    </div>
+  </div>
+
+    </div></div>
+  <script src="./script.js"></script>
+</body>
+<style>@import url("https://fonts.googleapis.com/css2?family=Play:wght@400;700&display=swap");
+p{
+  font-size:19px;
+  font-weight:800;
+  text-align:left;
+}
+  .main{
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+    width:100%;
+}
+body {
+  background-color: white;
+  
+
+  font-family: "Play", sans-serif;
+}
+
+.container {
+  width: 100%;
+}
+
+.packages {
+  margin: 20px;
+  width: 400px;
+  padding-bottom: 40px;
+  padding-top: 30px;
+  height: 100%;
+  background-color: #F7F7F7;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  border-radius: 20px;
+/*   box-shadow: 0 19px 38px rgba(30, 35, 33, 1), 0 15px 12px rgba(30, 35, 33, 0.2); */
+  flex-wrap: wrap;
+  color: #101010;
+}
+
+h1{
+  color: #101010;
+  font-size: 35px
+}
+h2 {
+  font-size: 30px;
+  color: #101010;
+}
+
+
+.list li {
+  font-size: 18px;
+  list-style: none;
+/*   border-bottom: 1px solid black; */
+  padding-inline-start: 0;
+  border-width: 1px;
+  padding: 10px;
+  text-align:left;
+}
+
+.first {
+  margin-top: 40px;
+  border-top: 1px solid #101010;
+}
+
+.list {
+  width: 80%;
+}
+
+ol,
+ul {
+  padding: 0;
+}
+
+.top {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+input,
+label {
+  display: inline-block;
+  vertical-align: middle;
+  margin: 10px 0;
+}
+
+.button {
+  padding: 10px 30px;
+  text-decoration: none;
+  font-size: 1.4em;
+  margin: 15px 15px;
+  border-radius: 50px;
+  color: #f4f4f4;
+  transition: all 0.3s ease 0s;
+}
+
+.button:hover {
+  transform: scale(1.2);
+}
+
+.button1 {
+  background-color: #2A7CFF;
+/*   box-shadow: 0 0 10px 0 #2A7CFF inset, 0 0 20px 2px #2A7CFF; */
+}
+
+.button2 {
+  background-color: #2A7CFF;
+/*   box-shadow: 0 0 10px 0 #2A7CFF inset, 0 0 20px 2px #2A7CFF; */
+}
+
+.button3 {
+  background-color: #2A7CFF;
+/*   box-shadow: 0 0 10px 0 #2A7CFF inset, 0 0 20px 2px #2A7CFF; */
+}
+
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #2A7CFF;
+  -webkit-transition: 0.4s;
+
+  box-shadow: 2px 6px 25px gray;
+  transform: translate(0px, 0px);
+  transition: 0.6s ease transform, 0.6s box-shadow;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+input:checked + .slider {
+  background-color: #50bfe6;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #50bfe6;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
+.package-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+</style>
+<script>
+function check() {
+  var checkBox = document.getElementById("checbox");
+  var monthlyPrices = document.getElementsByClassName("text2");
+  var annualPrices = document.getElementsByClassName("text1");
+
+  for (var i = 0; i < monthlyPrices.length; i++) {
+    if (checkBox.checked == true) {
+      // Calculate and display the yearly price with a 20% discount
+      let monthlyPrice = parseInt(monthlyPrices[i].innerText.replace(/\D/g, ''));
+      let annualPrice = Math.round(monthlyPrice * 12 * 0.8);
+      annualPrices[i].innerHTML = "&dollar;" + annualPrice + "<span style='font-size:19px;'>/year</span>";
+      annualPrices[i].style.display = "block";
+      monthlyPrices[i].style.display = "none";
+    } else {
+      // Display the monthly price
+      monthlyPrices[i].style.display = "block";
+      annualPrices[i].style.display = "none";
+    }
+  }
+}
+// Initial check to set the correct display
+check();
+</script>
